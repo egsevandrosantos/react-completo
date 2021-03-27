71ebb8d43da42f06e1621f8e3442dd99da837985
Evandro Santos <egs.evandro.santos@gmail.com>
2021-03-26 21:53:29 -0300

    0209 Eventos

--- 0206-ambiente-do-curso/appreact/src/App.js
+++ 0206-ambiente-do-curso/appreact/src/App.js
@@ -1,53 +1,23 @@
 import React from 'react';
 
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 const App = () => {
+  const handleClick = (event) => {
+    console.log(event.target);
+  };
+
+  const handleScroll = (event) => {
+    console.log(event);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
   return (
-    <>
-      {produtos
-        .filter(({ preco }) => +preco.replace('R$ ', '') > 1500)
-        .map((produto) => (
-          <React.Fragment key={produto.id}>
-            <h1>{produto.nome}</h1>
-            <p>Preço: {produto.preco}</p>
-            <ul>
-              {produto.cores.map((cor) => (
-                <li
-                  key={`${produto.id}${cor}`}
-                  style={{
-                    color: 'white',
-                    background: cor,
-                  }}
-                >
-                  {cor}
-                </li>
-              ))}
-            </ul>
-          </React.Fragment>
-        ))}
-    </>
+    <div style={{ height: '200vh' }}>
+      <button onClick={handleClick}>
+        {/* onMouseMove={handleClick} */}
+        Clique!
+      </button>
+    </div>
   );
 };
 
