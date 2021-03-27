679ab32c29ac08043df2dd6ddc58a8b89d189081
Evandro Santos <egs.evandro.santos@gmail.com>
2021-03-27 20:04:07 -0300

    0301 useState 2

--- 0206-ambiente-do-curso/appreact/src/App.js
+++ 0206-ambiente-do-curso/appreact/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+// import ButtonModal from './ButtonModal';
+// import Modal from './Modal';
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(true);
-  const [dados, setDados] = React.useState({ nome: 'André', idade: '30' });
-  const [modal, setModal] = React.useState(false);
+  // const [ativo, setAtivo] = React.useState(true);
+  // const [dados, setDados] = React.useState({ nome: 'André', idade: '30' });
+  // const [modal, setModal] = React.useState(() => window.localStorage.getItem('ativo'));
+  // const [modal, setModal] = React.useState(false);
+  // const [items, setItems] = React.useState('Teste');
+
+  // const handleClick = () => {
+  //   setAtivo(!ativo);
+  //   setDados({ ...dados, faculdade: 'Possui faculdade' });
+  //   // items = 'Outro'; // Errado
+  //   setItems('Outro'); // Correto
+  // };
+
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
 
   const handleClick = () => {
-    setAtivo(!ativo);
-    setDados({ ...dados, faculdade: 'Possui faculdade' });
+    setContar((contar) => {
+      // setItems((items) => [...items, 'Item ' + (contar + 1)]); // Isso cria efeito colateral (StrictMode)
+      return contar + 1;
+    });
+    setItems([...items, 'Item ' + (contar + 1)]);
   };
 
   return (
     <>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handleClick}>{contar}</button>
+
+      {/* <p>{items}</p>
+      <button onClick={handleClick}>Clicar</button>
       <p>{modal ? 'True' : 'False'}</p>
       <Modal modal={modal} setModal={setModal} />
       <ButtonModal setModal={setModal} />
       <p>{dados.nome}</p>
       <p>{dados.idade}</p>
       <p>{dados.faculdade}</p>
-      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
+      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button> */}
     </>
   );
 };
--- 0206-ambiente-do-curso/appreact/src/ButtonModal.js
+++ 0206-ambiente-do-curso/appreact/src/ButtonModal.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const ButtonModal = ({ setModal }) => {
-  return <button onClick={() => setModal(true)}>Abrir</button>;
+  function handleClick() {
+    // setModal((valorAnterior) => !valorAnterior); // Como callback temos acesso ao valor anterior sem nunca referenciar esse valor ou receber por props
+    setModal((modal) => {
+      if (modal) {
+        return false;
+      }
+
+      return true;
+    }); // Callback com função anonima
+  }
+
+  return <button onClick={handleClick}>Abrir</button>;
 };
 
 export default ButtonModal;
