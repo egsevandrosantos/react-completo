325832af36360c80f7d4c9961fe09a4343802113
Evandro Santos <egs.evandro.santos@gmail.com>
2021-04-03 13:55:40 -0300

    0401 Input 2

--- 0206-ambiente-do-curso/appreact/src/App.js
+++ 0206-ambiente-do-curso/appreact/src/App.js
@@ -1,14 +1,73 @@
 import React from 'react';
 
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+const formFields = [
+  { id: 'nome', label: 'Nome', type: 'text' },
+  { id: 'email', label: 'Email', type: 'email' },
+  { id: 'senha', label: 'Senha', type: 'password' },
+  { id: 'cep', label: 'Cep', type: 'number' },
+  { id: 'rua', label: 'Rua', type: 'text' },
+  { id: 'numero', label: 'Número', type: 'number' },
+  { id: 'bairro', label: 'Bairro', type: 'text' },
+  { id: 'cidade', label: 'Cidade', type: 'text' },
+  { id: 'estado', label: 'Estado', type: 'text' },
+];
+
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }, {})
+  );
+  // const [form, setForm] = React.useState({
+  //   nome: '',
+  //   email: '',
+  //   senha: '',
+  //   cep: '',
+  //   rua: '',
+  //   numero: '',
+  //   bairro: '',
+  //   cidade: '',
+  //   estado: '',
+  // });
+  const [request, setRequest] = React.useState({
+    response: null,
+    json: null,
+    error: null,
+    status: null,
   });
+
+  const sendPost = async () => {
+    let response, json, error, status;
+    try {
+      response = await fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(form),
+      });
+      if (!response.ok) {
+        throw await response.json();
+      }
+      json = await response.json();
+    } catch (err) {
+      error = err.message;
+    } finally {
+      status = response.status;
+      setRequest({ response, json, error, status });
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
+    sendPost();
   };
 
   const handleOnChange = ({ target }) => {
@@ -16,56 +75,55 @@ const App = () => {
     setForm({ ...form, [id]: value });
   };
 
+  // const capitalize = (text) => `${text[0].toUpperCase()}${text.slice(1)}`;
+
+  // const extractType = (key) => {
+  //   const emailsType = ['email'];
+  //   const passwordsType = ['senha'];
+  //   const numbersType = ['cep', 'numero'];
+  //   if (emailsType.includes(key)) return 'email';
+  //   if (passwordsType.includes(key)) return 'password';
+  //   if (numbersType.includes(key)) return 'number';
+  //   return 'text';
+  // };
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        name="nome"
-        type="text"
-        value={form.nome}
-        onChange={handleOnChange}
-      />
-      <p>{form.nome}</p>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        value={form.email}
-        onChange={handleOnChange}
-      />
-      <p>{form.email}</p>
+      {formFields.map(({ id, label, type }) => (
+        <React.Fragment key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input
+            type={type}
+            name={id}
+            id={id}
+            value={form[id]}
+            onChange={handleOnChange}
+            required={true}
+          />
+        </React.Fragment>
+      ))}
+      {/* {Object.keys(form).map((key) => (
+        <React.Fragment key={key}>
+          <label htmlFor={key}>{capitalize(key)}</label>
+          <input
+            type={extractType(key)}
+            name={key}
+            id={key}
+            value={form[key]}
+            onChange={handleOnChange}
+            required={true}
+          />
+        </React.Fragment>
+      ))} */}
+      {request.status && (
+        <p>
+          {request.status} -&nbsp;
+          {request.json ? 'Usuário criado com sucesso' : request.error}
+        </p>
+      )}
       <button type="submit">Enviar</button>
     </form>
   );
-
-  // const [nome, setNome] = React.useState('');
-  // const [email, setEmail] = React.useState('');
-
-  // return (
-  //   <form onSubmit={handleSubmit}>
-  //     <label htmlFor="nome">Nome</label>
-  //     <input
-  //       id="nome"
-  //       name="nome"
-  //       type="text"
-  //       value={nome}
-  //       onChange={({ target }) => setNome(target.value)}
-  //     />
-  //     <p>{nome}</p>
-  //     <label htmlFor="email">Email</label>
-  //     <input
-  //       type="email"
-  //       id="email"
-  //       name="email"
-  //       value={email}
-  //       onChange={({ target }) => setEmail(target.value)}
-  //     />
-  //     <p>{email}</p>
-  //     <button type="submit">Enviar</button>
-  //   </form>
-  // );
 };
 
 export default App;
