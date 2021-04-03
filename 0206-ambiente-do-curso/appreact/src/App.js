095b8679b8366d1c2983ba1c134de565d2fd6860
Evandro Santos <egs.evandro.santos@gmail.com>
2021-04-03 19:24:42 -0300

    0406 Componentes Input

--- 0206-ambiente-do-curso/appreact/src/App.js
+++ 0206-ambiente-do-curso/appreact/src/App.js
@@ -1,45 +1,15 @@
 import React from 'react';
-
-// Otimize o código do slide anterior
-// Utilizando a array abaixo para mostrar
-// cada checkbox na tela.
-
-const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+import Input from './Form/Input';
 
 const App = () => {
-  const [cores, setCores] = React.useState([]);
-
-  const handleChange = ({ target }) => {
-    if (target.checked) {
-      setCores([...cores, target.value]);
-    } else {
-      setCores(cores.filter((cor) => cor !== target.value));
-    }
-  };
-
-  const handleChecked = (cor) => cores.includes(cor);
-
-  // const capitalize = (text) => `${text[0].toUpperCase()}${text.slice(1)}`;
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
 
   return (
     <form>
-      {coresArray.map((cor) => (
-        <label key={cor} style={{ textTransform: 'capitalize' }}>
-          <input
-            type="checkbox"
-            value={cor}
-            checked={handleChecked(cor)}
-            onChange={handleChange}
-          />
-          {cor}
-          {/* {capitalize(cor)} */}
-        </label>
-      ))}
-      <ul>
-        {cores.map((cor) => (
-          <li key={cor}>{cor}</li>
-        ))}
-      </ul>
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
+      <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <button>Enviar</button>
     </form>
   );
 };
