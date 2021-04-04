ab3a69f627ca30b91c9056f1c3e9fa14e00cb44a
Evandro Santos <egs.evandro.santos@gmail.com>
2021-04-04 18:02:53 -0300

    Minha solucao - 0412 Desafio Formularios

--- 0206-ambiente-do-curso/appreact/src/App.js
+++ 0206-ambiente-do-curso/appreact/src/App.js
@@ -1,43 +1,96 @@
 import React from 'react';
-import Input from './Form/Input';
-import useForm from './Hooks/useForm';
+import Pergunta from './Pergunta';
+
+const perguntas = [
+  {
+    pergunta: 'Qual método é utilizado para criar componentes?',
+    options: [
+      'React.makeComponent()',
+      'React.createComponent()',
+      'React.createElement()',
+    ],
+    resposta: 'React.createElement()',
+    id: 'p1',
+  },
+  {
+    pergunta: 'Como importamos um componente externo?',
+    options: [
+      'import Component from "./Component"',
+      'require("./Component")',
+      'import "./Component"',
+    ],
+    resposta: 'import Component from "./Component"',
+    id: 'p2',
+  },
+  {
+    pergunta: 'Qual hook não é nativo?',
+    options: ['useEffect()', 'useFetch()', 'useCallback()'],
+    resposta: 'useFetch()',
+    id: 'p3',
+  },
+  {
+    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+    options: ['set', 'get', 'use'],
+    resposta: 'use',
+    id: 'p4',
+  },
+];
 
 const App = () => {
-  const cep = useForm('cep');
-  const email = useForm('email');
-  const nome = useForm();
-  const sobrenome = useForm(false);
-  const fieldsToValidate = [cep, email, nome, sobrenome];
+  const [pIndex, setPIndex] = React.useState(0);
+  const [respostas, setRespostas] = React.useState(
+    perguntas.reduce((acc, field) => ({ ...acc, [field.id]: '' }), {})
+  );
+  const [error, setError] = React.useState(null);
 
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-    const validations = fieldsToValidate.map((field) => field.validate());
-    if (!validations.some((valid) => !valid)) {
-      console.log('enviou');
-    } else {
-      console.log('não enviar');
+  const handleClick = () => {
+    let error;
+
+    const pergunta = perguntas[pIndex];
+    const resposta = respostas[pergunta.id];
+
+    if (!resposta || !resposta.toString().trim()) {
+      error = 'Selecione uma resposta';
     }
+
+    setError(error);
+    !error && setPIndex(pIndex + 1);
   };
 
+  const calcularAcertos = () =>
+    perguntas.reduce(
+      (acc, { resposta, id }) => acc + (resposta === respostas[id] ? 1 : 0),
+      0
+    );
+
   return (
-    <form onSubmit={handleSubmit}>
-      <Input label="Nome" id="nome" placeholder="Fulano" {...nome} />
-      <Input
-        label="Sobrenome"
-        id="sobrenome"
-        placeholder="De Tal"
-        {...sobrenome}
-      />
-      <Input label="CEP" id="cep" placeholder="00000-000" {...cep} />
-      <Input
-        label="Email"
-        id="email"
-        type="email"
-        placeholder="seu@email.com"
-        {...email}
-      />
-      <button>Enviar</button>
-    </form>
+    <>
+      {perguntas[pIndex] && (
+        <>
+          <Pergunta
+            pergunta={perguntas[pIndex]}
+            error={error}
+            setError={setError}
+            setRespostas={setRespostas}
+          />
+          <button onClick={handleClick}>Próxima</button>
+        </>
+      )}
+      {!perguntas[pIndex] && (
+        <>
+          <p>
+            {calcularAcertos()} acerto(s) de {perguntas.length} pergunta(s)
+          </p>
+          {perguntas.map((pergunta) => (
+            <React.Fragment key={pergunta.id}>
+              <h3>Pergunta: {pergunta.pergunta}</h3>
+              <p>Sua resposta: {respostas[pergunta.id]}</p>
+              <p>Resposta correta: {pergunta.resposta}</p>
+            </React.Fragment>
+          ))}
+        </>
+      )}
+    </>
   );
 };
 
